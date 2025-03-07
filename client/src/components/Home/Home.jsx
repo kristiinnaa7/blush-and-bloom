fd40a1bf802a2b3eb2538ab7ac9a54facf942ae2
Kristina <kristiinnaa7@gmail.com>
2025-03-07 13:02:17 +0200

    home design

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,6 +1,16 @@
+import "./Home.css";
+import home from '../../assets/home.jpg';
+import homehighlights from '../../assets/homehighlight.jpg';
+
 const Home = () => {
   return (
-    <div>HOME</div>
+    <div className="home-page">
+    <img className='home-bg' src={home} alt='home image'/>
+    <img className="home-highlights" src={homehighlights} alt='home highlights'/>
+    <h1>Where <strong>Fragrance</strong> Meets <strong>Elegance</strong></h1>
+    <p>Discover new worlds today</p>
+    <button>See all</button>
+</div>
   )
 };
 
