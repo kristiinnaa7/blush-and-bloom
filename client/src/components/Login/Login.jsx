6163a2abe836050ed9a2dfa07fc75106eb8dc8d9
Kristina <kristiinnaa7@gmail.com>
2025-03-10 11:35:11 +0200

    login form added

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -1,7 +1,100 @@
+import { useForm } from "../../hooks/useForm";
+import { useLogin } from "../../hooks/useAuth";
+import { Link, useNavigate } from 'react-router-dom'
+import { useState } from "react";
+
+import "./Login.css"
+
+import log from "../../assets/log.jpg";
+import logPhoto from "../../assets/bow.jpg"; // Ensure this image exists
+
+const initialValues = { email: "", password: "" }
+
 const Login = () => {
+
+  const [error, setError] = useState('');
+  const login = useLogin();
+  const navigate = useNavigate();
+
+  const loginHandler = async ({ email, password }) => {
+    try {
+      if (!email.trim()) {
+        setError('Email field is required.');
+        return;
+      }
+
+      if (!email.includes('@') || !email.includes('.')) {
+        setError('Invalid email format. Email must contain "@" and "."');
+        return;
+      }
+
+      if (!password.trim()) {
+        setError('Password field is required.');
+        return;
+      }
+
+      await login(email, password);
+      navigate('/');
+
+    } catch (err) {
+      setError(err.message);
+    }
+  };
+
+  const {
+    values,
+    changeHandler,
+    submitHandler
+  } = useForm(initialValues, loginHandler);
+
   return (
-    <div>Login</div>
-  )
+    <div className="wrapper-big">
+      <div className="border-img">
+        <img src={log} alt="Background" />
+      </div>
+      <section className="login">
+        <div className="form">
+          <h2>Login</h2>
+          
+          <div className="login-img">
+            <img src={logPhoto} alt="Login Image" />
+          </div>
+
+          <form className="login-form" onSubmit={submitHandler}>
+            <input
+              type="text"
+              name="email"
+              id="email"
+              value={values.email}
+              onChange={changeHandler}
+              placeholder="email"
+            />
+
+            <input
+              type="password"
+              name="password"
+              value={values.password}
+              onChange={changeHandler}
+              id="password"
+              placeholder="password"
+            />
+
+            {error && (
+              <p className="field">
+                <span className="error-message">{error}</span>
+              </p>
+            )}
+
+            <button className="button-login" type="submit">Login</button>
+
+            <p className="message">
+              Not registered? <Link to={"/register"}>Create an account</Link>
+            </p>
+          </form>
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default Login;
