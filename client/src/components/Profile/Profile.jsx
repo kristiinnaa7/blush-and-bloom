5ec6403019990572004d237b96dd3451450227b0
Kristina <kristiinnaa7@gmail.com>
2025-03-11 12:04:24 +0200

    profile updated

--- client/src/components/Profile/Profile.jsx
+++ client/src/components/Profile/Profile.jsx
@@ -1,8 +1,29 @@
 import { useAuthContext } from "../context/AuthContext";
-
+import "./Profile.css";
+import profile from "../../assets/profile.jpg";
+import frame from "../../assets/profileframe.jpg";
 const Profile = () => {
   const { email } = useAuthContext();
-  return <div>{email}</div>;
+  const name = email?.split("@")[0];
+  return (
+    <div className="profile">
+      <div className="frame">
+        <img src={frame} alt="frame" />
+      </div>
+
+      <div className="profile-info">
+        <h2>Profile</h2>
+        <div className="profile-fields">
+          <div>
+            <strong>Name:</strong> <span className="new-line">{name}</span>
+          </div>
+          <div>
+            <strong>Email:</strong> <span className="new-line">{email}</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Profile;
